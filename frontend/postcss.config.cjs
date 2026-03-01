55a3c8f2689f6109c6268e0fd7e5183612a3092b
Shubham <shubhamsharma68653@gmail.com>
2026-03-01 17:01:42 +0530

    poatcss corect

--- frontend/postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Use the new PostCSS adapter for Tailwind v4+: '@tailwindcss/postcss'
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
-  ],
-};
-
