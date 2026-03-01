07897501779b55f0473b409234d58e611f25042d
Shubham <shubhamsharma68653@gmail.com>
2026-03-01 15:18:45 +0530

    change vite for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: './', // ✅ important for static deploy
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8000', // your local backend for dev
         changeOrigin: true,
         secure: false,
       }
     }
-  },
+  }
 })
