234e411d7b064683cc92d6bcaffb6e9ec9bab5df
Shubham <shubhamsharma68653@gmail.com>
2026-03-02 19:45:12 +0530

    api change vite change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-
+  base: './',
   optimizeDeps: {
-    exclude: ["lightningcss"],
+    exclude: ['lightningcss'],
   },
-
-  build: {
-    outDir: "dist",
+  server: {
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
